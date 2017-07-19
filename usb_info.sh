98cf49fafd641e58f6f12829ee58cb20da218af2
Jack Conrad Kiefer II <jack.c.kiefer@gmail.com>
2017-07-19 14:34:52 -0600

    Update usb_info.sh

--- usb_info.sh
+++ usb_info.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# Gives the name of a recently plugged in USB device
+
 NAME=$(dmesg | tail | grep "] Mode" | cut -d']' -f2 | cut -d "[" -f 2 | tr -d '\n')
 echo "Your USB device's name is: /dev/$NAME"
