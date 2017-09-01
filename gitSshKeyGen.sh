003a338c60e356306c3f2592402ea564e3a40999
Philip Nelson <PhilipNelson5@gmail.com>
2017-08-31 21:47:57 -0600

    this script will generate a new ssh key and coppy it to your clipboard. Used for setting up git on new machines

---
+++ gitSshKeyGen.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+if (( $# == 1 )); then
+  EMAIL=$1
+else
+  echo -e "${RED}Enter email as first argument${NC}"
+  exit
+fi
+
+ssh-keygen -t rsa -b 4096 -C $EMAIL
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/id_rsa
+
+if hash xclip 2>/dev/null; then
+  echo -e "${GREEN}SSH Key coppied to clipboard${NC}"
+  xclip -selection c < ~/.ssh/id_rsa.pub
+  echo -e "${GREEN}[DONE]${NC}"
+  exit
+else
+  echo -e "${RED}xclip not installed${NC}"
+  echo -e "${GREEN}Coppy key from terminal${NC}"
+  cat ~/.ssh/id_rsa.pub
+  echo -e "${GREEN}[DONE]${NC}"
+  exit
+fi
+
+exit
