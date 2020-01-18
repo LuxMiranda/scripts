74a242bbd86533cb662566d88cefe9c05137bddd
Philip Nelson <PhilipNelson5@gmail.com>
2020-01-18 15:20:53 -0700

    improves the script to prompt for email if not given as argument

--- git_ssh_key_gen.sh
+++ git_ssh_key_gen.sh
@@ -7,8 +7,7 @@ RED='\033[0;31m'
 if (( $# == 1 )); then
   EMAIL=$1
 else
-  echo -e "${RED}Enter email as first argument${NC}"
-  exit
+  read -p 'enter your email: ' EMAIL
 fi
 
 ssh-keygen -t rsa -b 4096 -C $EMAIL
@@ -18,16 +17,16 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
 if hash xclip 2>/dev/null; then
-  echo -e "${GREEN}SSH Key coppied to clipboard${NC}"
+  echo -e "${GREEN}SSH Key copied to clipboard${NC}"
   xclip -selection c < ~/.ssh/id_rsa.pub
-  echo -e "${GREEN}[DONE]${NC}"
-  exit
+else if hash xsel 2>/dev/null; then
+  echo -e "${GREEN}SSH Key copied to clipboard${NC}"
+  xsel -b < ~/.ssh/id_rsa.pub
 else
   echo -e "${RED}xclip not installed${NC}"
-  echo -e "${GREEN}Coppy key from terminal${NC}"
+  echo -e "${GREEN}Copy key from terminal${NC}"
   cat ~/.ssh/id_rsa.pub
-  echo -e "${GREEN}[DONE]${NC}"
-  exit
 fi
 
+echo -e "${GREEN}[DONE]${NC}"
 exit
